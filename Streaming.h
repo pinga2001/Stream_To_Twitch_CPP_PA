4ee4ba89049635a13b46f258895d85c6a5f3ccaa
pinga2001 <70075269+pinga2001@users.noreply.github.com>
2020-08-22 15:48:49 -0500

    Update Streaming.h

--- Streaming.h
+++ Streaming.h
@@ -1,1 +1,13 @@
+#include <string>
+#include <vector>
+#include <exception>
+#include <stdio.h>
+#include <stdexcept>
+#include <iostream>
+#include <fstream>
+#include <math.h>
+#include <algorithm>
+#include <sstream>
+#include <functional>
 
+using namespace std;
